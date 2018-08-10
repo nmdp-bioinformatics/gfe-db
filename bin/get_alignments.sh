332f9aa771fe8be8aac683568ea31e69f2aafc21
Mike Halagan <mhalagan@nmdp.org>
2018-08-10 11:56:16 -0500

    Updated get_alignments file

--- bin/get_alignments.sh
+++ bin/get_alignments.sh
@@ -29,6 +29,9 @@ for dbversion in `python -c 'import pandas as pd;df = pd.read_html("https://www.
 		perl -ne 'END{foreach my $line (@data){if($line !~ /^\w+\d{0,1}\*/){print $line,"\n";}else{my($id,$s_seq) = split(/\s+/,$line);my $n = length($s_seq);if($n != $max){ my $add = "." x ($max - $n); $s_seq = $s_seq.$add;}print sprintf("%-".$space."s %".$max."s",$id,$s_seq),"\n";}}}chomp;push(@data,$_);next if($_ !~ /^\w+\d{0,1}\*/);my $n = length($_);my($id,$s_seq) = split(/\s+/,$_);$space = ($n - (length($id) + length($s_seq))) + length($id) -1;$max = length($s_seq) > $max ? length($s_seq) : $max;' ${DATA_DIR}/${dbversion_trimmed}"/"${loc}"_tmp.sth" > ${DATA_DIR}/${dbversion_trimmed}"/"${loc}"_prot.sth"
 		rm ${DATA_DIR}/${dbversion_trimmed}/${loc}"_tmp.msf"
 		rm ${DATA_DIR}/${dbversion_trimmed}/${loc}"_tmp.sth"
+
+		echo ${DATA_DIR}/${dbversion_trimmed}"/"${loc}
+		ls -rtl ${DATA_DIR}/${dbversion_trimmed}/*.sth
 	done
 done
 
