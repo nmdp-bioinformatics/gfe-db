00892c1e3b75fcd6ea6407ff71ab3207979d005e
Gregory Lindsey <gclindsey@gmail.com>
2021-03-07 21:24:14 -0800

    load complete schema with multiple releases

--- bin/load_db.sh
+++ bin/load_db.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+CYPHER=load_duplicate_rels.cyp
+
+echo "Load script: $CYPHER"
 # TO DO: format IMGTHLA version string
 for RELEASE in $(echo $RELEASES | sed "s/,/ /g")
 do
     echo "Loading release version $RELEASE..."
-    cat neo4j/load_new.cyp | \
+    cat neo4j/$CYPHER | \
         sed "s/RELEASE/$RELEASE/g" | \
         docker exec --interactive gfe cypher-shell -u neo4j -p gfedb
 done
