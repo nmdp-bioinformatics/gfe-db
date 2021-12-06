dfeac75e19729f66a243179efcb04d11aa616a9a
Gregory Lindsey <gclindsey@gmail.com>
2021-12-05 16:21:36 -0800

    update trigger lambda

--- trigger/trigger/app.py
+++ trigger/trigger/app.py
@@ -1,11 +1,12 @@
 import os
 import logging
 import datetime
+import copy
 import json
+import re
 import requests
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv());
+import numpy as np
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -13,60 +14,175 @@ logger.setLevel(logging.INFO)
 GITHUB_PERSONAL_ACCESS_TOKEN = os.environ["GITHUB_PERSONAL_ACCESS_TOKEN"]
 GITHUB_REPOSITORY_OWNER = os.environ["GITHUB_REPOSITORY_OWNER"]
 GITHUB_REPOSITORY_NAME = os.environ["GITHUB_REPOSITORY_NAME"]
+GFE_BUCKET = os.environ["GFE_BUCKET"]
 
-# Time in seconds between scheduled invocations
-LAMBDA_INVOCATION_INTERVAL_SECONDS = int(os.environ["LAMBDA_INVOCATION_INTERVAL_SECONDS"])
-
-base_url = 'https://api.github.com'
-
-headers = {
-    'Authorization': f'token {GITHUB_PERSONAL_ACCESS_TOKEN}',
-    'Content-Type': 'application/json',
-    'Accept': 'application/vnd.github.v3+json'
-}
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
+    """Checks for new IMGT/HLA releases and triggers the update
+    pipeline if any are found"""
 
-    # Get latest release version
-    endpoint = f'/repos/{GITHUB_REPOSITORY_OWNER}/{GITHUB_REPOSITORY_NAME}/branches?per_page=100'
-    url = "".join([base_url, endpoint])
-    response = requests.get(url)
-    latest_release_version = json.loads(response.content)[-2]['name']
+    logger.info(json.dumps(event))
 
-    logger.info(f"latest_release_version: {latest_release_version}")
+    # Load the previous repository state
+    branches_config_path = f"s3://{GFE_BUCKET}/config/trigger/IMGTHLA-repository-state.json"
+    branches_config = read_config(branches_config_path)
+    previous_state = branches_config['releases']
 
-    # Get most recent commit sha and date
-    endpoint = f'/repos/{GITHUB_REPOSITORY_OWNER}/{GITHUB_REPOSITORY_NAME}/commits?per_page=100'
-    url = "".join([base_url, endpoint])
-    response = requests.get(url)
-    commits = json.loads(response.content)
+    # Get the current repository state
+    current_state = get_releases(GITHUB_REPOSITORY_OWNER, GITHUB_REPOSITORY_NAME)
 
-    logger.info(f"commits: {commits}")
+    # Log repository states
+    logger.info(f'Previous repository state:\n{json.dumps(previous_state)}')
+    logger.info(f'Current repository state:\n{json.dumps(current_state)}')
 
-    most_recent_commit_sha = commits[0]['sha']
-    most_recent_commit_date = commits[0]['commit']['author']['date']
-    most_recent_commit_date = datetime.datetime.strptime(most_recent_commit_date, "%Y-%m-%dT%H:%M:%SZ")
-    utc_now = datetime.datetime.utcnow()
-    time_delta = utc_now - most_recent_commit_date
+    # Compare the previous state with the current state
+    new_releases = check_new_releases(previous_state, current_state)
+    
+    if new_releases:
+    
+        logger.info(f'Pipeline triggered:')
 
-    logger.info(f"commits: {commits}")
+        # Load the current pipeline params
+        params_path = f"s3://{GFE_BUCKET}/config/pipeline/params.json"
+        params = read_config(params_path)
 
-    # If the time passed since the last commit is less than 3600 seconds, trigger the pipeline
-    run_pipeline = time_delta.total_seconds() < LAMBDA_INVOCATION_INTERVAL_SECONDS
+        logger.info(f'Running gfe-db update pipeline with these params:\n{json.dumps(params)}')
 
-    # List files from most recent commit
-    endpoint = f'/repos/{GITHUB_REPOSITORY_OWNER}/{GITHUB_REPOSITORY_NAME}/contents?ref={most_recent_commit_sha}'
-    url = "".join([base_url, endpoint])
-    response = requests.get(url)
-    contents = json.loads(response.content)
-    hla_dat = list(filter(lambda item: item['name'] == 'hla.dat', contents))
+        state_machine_input = []
 
-    # if hla.dat is in the most recent commit, check if the commit occurred after the last invocation
-    if run_pipeline:
-        print("Running pipeline...")
+        for release in new_releases:
+            
+            params_input = copy.deepcopy(params["params"]["environment"])
+            params_input["RELEASES"] = release
+            logger.info(f'{params_input}')            
+            state_machine_input.append(params_input)
+
+    # Always update the config file
+    write_config(branches_config_path)
 
     return
 
 
-# if __name__ == "__main__":
-#     lambda_handler("","")
+def get_branches(owner, repo):
+    """Return a list of GitHub branches for the specified repository"""
+
+    base_url = 'https://api.github.com'
+
+    # Endpoint
+    endpoint = f'/repos/{owner}/{repo}/branches?per_page=100'
+
+    url = base_url + endpoint
+
+    # Headers
+    headers = {
+        'Authorization': f'token {GITHUB_PERSONAL_ACCESS_TOKEN}', 
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    
+    response = requests.get(url)
+    branches = json.loads(response.content)
+    
+    return [branch["name"] for branch in branches]
+
+
+def is_valid_release(branch):
+    """Returns True if the branch is a valid release, False if not"""
+
+    # IMGT/HLA release format string
+    # Checks for a pattern corresponding to 3 digits followed by one zero, ie., 3460
+    release_pattern = r'^\d{3}0$'
+    p = re.compile(release_pattern)
+    match = p.match(branch)
+
+    if match:
+        return True
+    else:
+        return False
+
+
+def get_releases(owner, repo):
+    """Filters repository branches for those that match the IMGT/HLA release format string"""
+    return list(filter(is_valid_release, get_branches(owner, repo)))
+
+
+def write_config(path):
+    """Writes config file containing the current state of branches in 
+    a GitHub repo"""
+    
+    branches_config = {
+        "timestamp": str(datetime.datetime.utcnow())[:-7],
+        "repository_url": f"https://github.com/{GITHUB_REPOSITORY_OWNER}/{GITHUB_REPOSITORY_NAME}",
+        "releases": get_releases(GITHUB_REPOSITORY_OWNER, GITHUB_REPOSITORY_NAME)
+    }
+    
+    try:
+        response = s3.put_object(
+             Body=json.dumps(branches_config),
+             Bucket=GFE_BUCKET,
+             Key="/".join(path.split("/")[3:]))
+        
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            logger.info(f"Wrote config file to {path}")
+            return
+        else:
+            logger.error(f'Failed to write config file to {path}. HTTPStatusCode: {response["ResponseMetadata"]["HTTPStatusCode"]}')
+            return
+        
+    except Exception as err:
+        raise err
+
+
+def read_config(path):
+    """Reads config file containing the current state of branches in 
+    a GitHub repo"""
+    
+    try:
+        response = s3.get_object(
+            Bucket=GFE_BUCKET, 
+            Key="/".join(path.split("/")[3:]))
+        
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            logger.info(f"Read config file from {path}")
+            return json.loads(response["Body"].read().decode())
+        else:
+            logger.error(f'Failed to read config file to {path}. HTTPStatusCode: {response["ResponseMetadata"]["HTTPStatusCode"]}')
+            return
+        
+    except Exception as err:
+        raise err
+
+
+def check_new_releases(previous_state, current_state):
+    """Compares the previous repository state as a list of branches, with the current
+    repository state. Returns new branches if they are a valid IMGT/HLA release, None 
+    if not."""
+
+    # Check if any branches have been added
+    new_branches_count = len(current_state) - len(previous_state)
+    branches_added = (new_branches_count > 0)
+
+    if branches_added:
+
+        logger.info(f"New branches: {current_state[-new_branches_count:]}")
+
+        # Get the new branches
+        new_releases = sorted([int(release) for release in list(set(current_state).difference(previous_state))])
+        previous_state_last_release = [int(previous_state[-1])]
+
+        # Check that the last release of the previous state and the newly added releases all differ by 10
+        elementwise_difference = list(set(np.diff([release for release in previous_state_last_release + new_releases])))
+        new_branches_are_valid_releases = (len(elementwise_difference) == 1 and elementwise_difference[0] == 10)
+
+        if new_branches_are_valid_releases:
+            
+            return new_releases
+    else:
+        logger.info("No new branches detected")
+        
+        return
+
+
+if __name__ == "__main__":
+    lambda_handler("","")
