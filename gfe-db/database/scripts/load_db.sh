ac9d30ecf5b0d9e1ff9f4bb8f2dc55a79fce667e
Gregory Lindsey <gclindsey@gmail.com>
2022-04-08 16:58:11 -0700

    fetch username/password in load_db.sh

--- gfe-db/database/scripts/load_db.sh
+++ gfe-db/database/scripts/load_db.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+
+# Set absolute paths
 NEO4J_CYPHER_PATH_S3=config/neo4j/cypher
 NEO4J_CYPHER_PATH=/var/lib/neo4j/cypher
 NEO4J_IMPORT_PATH=/var/lib/neo4j/import
 
+# Check for release argument
 RELEASE=$1
+
 if [[ -z $RELEASE ]]; then
     echo "Release version not found"
     exit 1
@@ -12,8 +17,14 @@ else
     echo "Starting load process for $RELEASE"
 fi
 
+# Get Neo4j Credentials
+NEO4J_CREDENTIALS=$(aws secretsmanager get-secret-value \
+    --region $REGION \
+    --secret-id gfe-db-dev-Neo4jCredentials | jq -r '.SecretString')
+NEO4J_USERNAME=$(echo $NEO4J_CREDENTIALS | jq -r '.NEO4J_USERNAME')
+NEO4J_PASSWORD=$(echo $NEO4J_CREDENTIALS | jq -r '.NEO4J_PASSWORD')
 
-NEO4J_PASSWORD=gfedb
+# Get data bucket name
 DATA_BUCKET_NAME=$(aws ssm get-parameters \
     --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/') \
     --names "/gfe-db/dev/us-east-1/DataBucketName" \
@@ -31,22 +42,24 @@ echo "Fetching most recent Cypher scripts"
 aws s3 cp --recursive s3://$DATA_BUCKET_NAME/$NEO4J_CYPHER_PATH_S3/ $NEO4J_CYPHER_PATH
 
 # Download data to /var/lib/neo4j/import
-echo "Downloading data for release $RELEASE"
+echo "Downloading CSV data for release $RELEASE"
 aws s3 cp --recursive s3://$DATA_BUCKET_NAME/data/$RELEASE/csv/ $NEO4J_IMPORT_PATH/
 
 # Update Cypher load query for correct release
 mkdir -p $NEO4J_CYPHER_PATH/tmp/$RELEASE/
 cat /var/lib/neo4j/cypher/load.cyp | sed "s/RELEASE/$RELEASE/g" > $NEO4J_CYPHER_PATH/tmp/$RELEASE/load.$RELEASE.cyp
 
+printf "Updated script for release $RELEASE:\n$(cat $NEO4J_CYPHER_PATH/tmp/$RELEASE/load.$RELEASE.cyp)"
+
 # Run Cypher load query
 echo "Loading data for release $RELEASE into Neo4j..."
 cat $NEO4J_CYPHER_PATH/tmp/$RELEASE/load.$RELEASE.cyp | \
     /usr/bin/cypher-shell \
-        --username neo4j \
+        --username $NEO4J_USERNAME \
         --password $NEO4J_PASSWORD \
         --format verbose
 
-# Conditional queries
+# TODO: Conditional queries for alignments, KIR (requires running separate Cypher scripts)
 # if $ALIGN; then \
     # load alignments
 
