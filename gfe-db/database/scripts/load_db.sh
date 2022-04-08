ef54a726a4e7f7761c044af2f88c9aa32a2215e6
Gregory Lindsey <gclindsey@gmail.com>
2022-04-08 14:11:49 -0700

    parameterize load script; add SSM parameter

--- gfe-db/database/scripts/load_db.sh
+++ gfe-db/database/scripts/load_db.sh
@@ -4,8 +4,15 @@ NEO4J_CYPHER_PATH_S3=config/neo4j/cypher
 NEO4J_CYPHER_PATH=/var/lib/neo4j/cypher
 NEO4J_IMPORT_PATH=/var/lib/neo4j/import
 
-# Parse from JSON or Secrets Manager
-RELEASE=3470
+RELEASE=$1
+if [[ -z $RELEASE ]]; then
+    echo "Release version not found"
+    exit 1
+else
+    echo "Starting load process for $RELEASE"
+fi
+
+
 NEO4J_PASSWORD=gfedb
 DATA_BUCKET_NAME=$(aws ssm get-parameters \
     --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/') \
@@ -25,7 +32,7 @@ aws s3 cp --recursive s3://$DATA_BUCKET_NAME/$NEO4J_CYPHER_PATH_S3/ $NEO4J_CYPHE
 
 # Download data to /var/lib/neo4j/import
 echo "Downloading data for release $RELEASE"
-aws s3 cp --recursive s3://$DATA_BUCKET_NAME/data/3470/csv/ $NEO4J_IMPORT_PATH/
+aws s3 cp --recursive s3://$DATA_BUCKET_NAME/data/$RELEASE/csv/ $NEO4J_IMPORT_PATH/
 
 # Update Cypher load query for correct release
 mkdir -p $NEO4J_CYPHER_PATH/tmp/$RELEASE/
