e7f3f37b61afc63b03f07c96a1105c9c8ba26f0d
Gregory Lindsey <gclindsey@gmail.com>
2024-01-08 19:18:19 -0800

    orchestrate local gfe-db build using make targets

--- gfe-db/local/build-local.sh
+++ gfe-db/local/build-local.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -eu
 
-from_path=s3://nmdpf-gfe-db-810526023897-us-east-1/backups/neo4j/2024/01/07/20/18/gfedb.zip # $1
+# TODO create a version tag from the data path for Docker, push both latest and versioned tags
+DATA_S3_PATH=$1
+TEMP_DIR=/tmp/$APP_NAME
+
+# TODO validate the path using the same logic as the Makefile
 
 # Create volumes to mount
-mkdir -p /tmp/restore neo4j/{data,logs,backups/{system,neo4j}}
+mkdir -p $TEMP_DIR neo4j/{data,logs,backups/{system,neo4j}}
 
 # Download most recent backup
-aws s3 cp $from_path /tmp/restore/gfedb.zip
-unzip -o /tmp/restore/gfedb.zip -d neo4j
+aws s3 cp $DATA_S3_PATH $TEMP_DIR/gfedb.zip
+unzip -o $TEMP_DIR/gfedb.zip -d neo4j
 
 # Load backup into volumes
 docker run --interactive --tty --rm \
@@ -24,5 +28,11 @@ docker run --interactive --tty --rm \
     neo4j/neo4j-admin:5.15.0 \
     neo4j-admin database load --from-path=/backups/neo4j --overwrite-destination=true --verbose neo4j
 
-# Build image
+# Build, tag and push image
 docker build -t gfe-db .
+docker tag gfe-db:latest "$DOCKER_USERNAME/gfe-db:latest"
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker push "$DOCKER_USERNAME/gfe-db:latest"
+
+# Clean up
+rm -rf neo4j/{data,backups} /tmp/$APP_NAME
