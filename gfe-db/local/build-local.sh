9f8e159e120ceed7fe04941734eec23b7bd8f131
Gregory Lindsey <gclindsey@gmail.com>
2024-01-11 17:46:50 -0800

    use 'gfedb' as default database name for Docker image

--- gfe-db/local/build-local.sh
+++ gfe-db/local/build-local.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -eu
 
-# TODO validate the data path using the same logic as in the Makefile restore target
 DATA_S3_PATH=$1
 TEMP_DIR=/tmp/$APP_NAME
+
+# Check that NEO4J_DATABASE_NAME is set
+if [ -z ${NEO4J_DATABASE_NAME+x} ]; then
+    echo "NEO4J_DATABASE_NAME is not set" >&2
+    exit 1
+else
+    echo "NEO4J_DATABASE_NAME is set to '$NEO4J_DATABASE_NAME'"
+fi
 
 format_s3_date() {
     local s3_path="$1"
@@ -16,21 +23,21 @@ format_s3_date() {
         local minute=${BASH_REMATCH[5]}
 
         # Combine the parts into the desired format
-        echo "${year}${month}${day}${hour}${minute}"
+        echo "${year}${month}${day}_${hour}${minute}"
     else
         echo "Could not parse date from S3 path: $s3_path" >&2
         exit 1
     fi
 }
 
-echo "$(format_s3_date $DATA_S3_PATH)"
+echo "This version is $(format_s3_date $DATA_S3_PATH)"
 
 # Create volumes to mount
-mkdir -p $TEMP_DIR neo4j/{data,logs,backups/{system,neo4j}}
+mkdir -p $TEMP_DIR neo4j/{data,logs,backups/{system,$NEO4J_DATABASE_NAME}}
 
 # Download most recent backup
-aws s3 cp $DATA_S3_PATH $TEMP_DIR/gfedb.zip
-unzip -o $TEMP_DIR/gfedb.zip -d neo4j
+aws s3 cp $DATA_S3_PATH $TEMP_DIR/$NEO4J_DATABASE_NAME.zip
+unzip -o $TEMP_DIR/$NEO4J_DATABASE_NAME.zip -d neo4j
 
 # Load backup into volumes
 docker run --interactive --tty --rm \
@@ -45,11 +52,11 @@ docker run --interactive --tty --rm \
     --volume=$(pwd)/neo4j/data:/data \
     --volume=$(pwd)/neo4j/backups:/backups \
     neo4j/neo4j-admin:5.15.0 \
-    neo4j-admin database load --from-path=/backups/neo4j --overwrite-destination=true --verbose neo4j
+    neo4j-admin database load --from-path=/backups/$NEO4J_DATABASE_NAME --overwrite-destination=true --verbose $NEO4J_DATABASE_NAME
 
 # Build, tag and push image
 version_tag=$(format_s3_date $DATA_S3_PATH)
-docker build -t gfe-db .
+docker build --build-arg NEO4J_DATABASE_NAME=$NEO4J_DATABASE_NAME -t gfe-db .
 docker tag gfe-db:latest $DOCKER_USERNAME/gfe-db:latest
 docker tag gfe-db:latest $DOCKER_USERNAME/gfe-db:$version_tag
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
