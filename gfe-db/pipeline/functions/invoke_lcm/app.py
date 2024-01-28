2351713542668d0f4080156465b49bc2ab26ef4b
Gregory Lindsey <gclindsey@gmail.com>
2024-01-27 20:22:57 -0800

    rename function path

--- gfe-db/pipeline/functions/invoke_lcm/app.py
+++ gfe-db/pipeline/functions/invoke_lcm/app.py
@@ -1,8 +1,8 @@
 """This function is invoked through SNS when GfeDbLoadQueueHasMessagesAlarm is triggered from messages present in the GfeDbLoadQueue.
-It's only responsibility is to invoke the LoadConcurrencyHandler state machine which maintains a concurrency of 1 for loading Neo4j
+It's only responsibility is to invoke the LoadConcurrencyManager state machine which maintains a concurrency of 1 for loading Neo4j
 to avoid clashes with concurrent executions in GfeDbUpdatePipeline. This allows GfeDbUpdatePipeline to run data builds at concurrency > 1
-and keeps data loads at concurrency = 1. The LoadConcurrencyHandler will end the execution when GfeDbLoadQueueHasMessagesAlarm enters 
-OK state, meaning there are no more messages in the queue. All requests for loading Neo4j are handled by the LoadConcurrencyHandler 
+and keeps data loads at concurrency = 1. The LoadConcurrencyManager will end the execution when GfeDbLoadQueueHasMessagesAlarm enters 
+OK state, meaning there are no more messages in the queue. All requests for loading Neo4j are handled by the LoadConcurrencyManager 
 state machine.
 """
 
@@ -15,6 +15,7 @@ if __name__ != "app":
     sys.path.append(os.environ["GFEDBMODELS_PATH"])
 
 import logging
+from datetime import datetime
 import json
 from gfedbmodels.constants import (
     session,
@@ -32,8 +33,9 @@ AWS_REGION = os.environ["AWS_REGION"]
 # Boto3 Clients
 states = session.client("stepfunctions", region_name=AWS_REGION)
 
-# # TODO Get SSM Parameters
-# lch_state_machine_arn = pipeline.params.LoadConcurrencyHandlerStateMachineArn
+# Get SSM Parameters
+update_pipeline_state_machine_arn = pipeline.params.UpdatePipelineStateMachineArn
+lcm_state_machine_arn = pipeline.params.LoadConcurrencyManagerStateMachineArn
 
 def lambda_handler(event, context):
 
@@ -52,6 +54,14 @@ def lambda_handler(event, context):
         # TODO query the state table for commits with PENDING status
 
         # TODO trigger the Load Concurrency Handler state machine
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        execution_id = update_pipeline_state_machine_arn.split(":")[-1] + "_" + timestamp
+
+        response = states.start_execution(
+            stateMachineArn=lcm_state_machine_arn,
+            name=execution_id,
+            # input=json.dumps({}),
+        )
         pass
 
     return
